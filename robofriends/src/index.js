58724c6e05a18773aa6ff73525cbadc7f2fe60fc
Inkslinger9 <96968798+Inkslinger9@users.noreply.github.com>
2022-10-19 12:47:14 -0400

    Workin on it

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import CardList from './CardList';
+import { robots } from './robots';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+     <CardList robots = {robots}/>
+    </div>
+    
   </React.StrictMode>
 );
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
